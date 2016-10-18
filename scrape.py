1f80690f560c98a4e4e5b926c95a7b992fbeab90
Constantin Scholl <constantin@scholl.im>
2016-10-17 22:18:21 -0400

    finish

--- scrape.py
+++ scrape.py
@@ -11,6 +11,14 @@ rows = [tr.find_all('td') for tr in table.find_all('tr')]
 cropped_rows = rows[1:-1]
 
 extract = [row[4].text for row in cropped_rows]
-neighborhoods = np.hstack([element.split(', ') for element in extract])
+neighborhoods = np.hstack([element.split(', ') for element in extract]).tolist()
+values = zip([el for el in range(len(neighborhoods))], neighborhoods)
 
-print(neighborhoods)
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+c.execute("CREATE TABLE neighborhoods (id INTEGER PRIMARY KEY, neighborhood TEXT)")
+c.executemany("INSERT INTO neighborhoods VALUES (?, ?)", values)
+conn.commit()
+
+c.execute("SELECT * FROM neighborhoods")
+c.fetchall()
