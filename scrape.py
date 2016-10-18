9a617db1b3ad537ce2903029a130da6b76c25581
Constantin Scholl <constantin@scholl.im>
2016-10-17 20:35:16 -0400

    first class

--- scrape.py
+++ scrape.py
@@ -1,7 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
+import numpy as np
 
 response = requests.get('https://en.wikipedia.org/wiki/Neighborhoods_in_New_York_City')
-
 soup = BeautifulSoup(response.text, 'html.parser')
+
+table = soup.find('table')
+rows = [tr.find_all('td') for tr in table.find_all('tr')]
+cropped_rows = rows[1:-1]
+
+extract = [row[4].text for row in cropped_rows]
+neighborhoods = np.hstack([element.split(', ') for element in extract])
+
+print(neighborhoods)
